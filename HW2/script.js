c26bf85f7c709ce928b2ef4c6acead4c7aac5a46
Mikayel <mikael.bejanyan@gmail.com>
2022-04-24 23:55:31 +0400

    Added some code for the version without the diagonals but yet incomplete

--- HW2/script.js
+++ HW2/script.js
@@ -25,11 +25,15 @@ const rotate = (matrix, deg) => {
 
 // 2. Rotate all matrix elements except the diagonals
 const rotateWithoutDiagonal = (matrix, deg) => {
-  // code here
+  const rotations = calculateRotations(deg);
+
+  return Array.from({ length: rotations }).reduce((a) => {
+    return rotate90DegNoDiagonals(a);
+  }, matrix);
 };
 
 //------------------ Helper Functions ------------------
-
+/////// For rotate
 // the inclass version
 const rotate90Deg = (matrix) => {
   let matrixNew = [];
@@ -48,6 +52,29 @@ const rotate90Deg = (matrix) => {
   return matrixNew;
 };
 
+/////// For rotateWithoutDiagonal
+const rotate90DegNoDiagonals = (matrix) => {
+  let matrixNew = [];
+  let innerArr = matrix[0];
+
+  for (let j = 0; j < innerArr.length; j++) {
+    let tmpArr = [];
+
+    for (let i = matrix.length - 1; i >= 0; i--) {
+      if (j == i) {
+        break;
+      } else {
+        tmpArr.push(matrix[i][j]);
+      }
+    }
+
+    matrixNew.push(tmpArr);
+  }
+
+  return matrixNew;
+};
+
+/////// For both
 const calculateRotations = (deg) => {
   if (deg % 90 !== 0) {
     throw new RangeError("Incorrect value");
@@ -60,9 +87,18 @@ const calculateRotations = (deg) => {
 
 //-----------------Tests--------------------
 
+//// with diagonal changes
 console.log(matrix);
 // console.log(rotate90Deg(matrix));
 console.log(rotate(matrix, 180));
+console.log(matrix2);
+// console.log(rotate90Deg(matrix2));
+console.log(rotate(matrix2, 90));
+
+// without diagonal changes
+console.log(matrix);
+// console.log(rotate90Deg(matrix));
+console.log(rotateWithoutDiagonal(matrix, 90));
 console.log(matrix2);
 // console.log(rotate90Deg(matrix2));
 console.log(rotate(matrix2, 90));

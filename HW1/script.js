dde2875d01b30ffbb4a9cd531cc70f82f1b652eb
Mikayel <mikael.bejanyan@gmail.com>
2022-04-24 20:16:40 +0400

    Fixed HW1: instead of passing the array as an argument, changed to using 'this'

--- HW1/script.js
+++ HW1/script.js
@@ -9,115 +9,119 @@ Array.prototype.map = null;
 
 // Implement these methods
 
-Array.prototype.find = function (arr, callbackFN) {
+/*  Note 
+I had all the arrays (on which the methods shuold have been called) as an argument, then I realized that it shouldn't be called as an argument but that I should use 'this' instead, hopefully that's correct
+*/
+
+Array.prototype.find = function (callbackFN) {
   // implementation
 
-  const index = this.findIndex(arr, callbackFN);
+  const index = this.findIndex(callbackFN);
 
   if (index === -1) return undefined;
 
-  return arr[index];
+  return this[index];
 };
 
-Array.prototype.findIndex = function (arr, callbackFN) {
+Array.prototype.findIndex = function (callbackFN) {
   // implementation
 
-  for (let i = 0; i < arr.length; i++) {
-    if (callbackFN(arr[i], i, arr)) return i;
+  for (let i = 0; i < this.length; i++) {
+    if (callbackFN(this[i], i, this)) return i;
   }
   return -1;
 };
 
-Array.prototype.lastIndexOf = function (arr, value) {
+Array.prototype.lastIndexOf = function (value) {
   // implementation
 
-  for (let i = arr.length - 1; i >= 0; i--) {
-    if (arr[i] === value) return i;
+  for (let i = this.length - 1; i >= 0; i--) {
+    if (this[i] === value) return i;
   }
   return -1;
 };
 
-Array.prototype.some = function (arr, callbackFN) {
+Array.prototype.some = function (callbackFN) {
   // implementation
 
-  for (let i = 0; i < arr.length; i++) {
-    if (callbackFN(arr[i], i, arr)) return true;
+  for (let i = 0; i < this.length; i++) {
+    if (callbackFN(this[i], i, this)) return true;
   }
   return false;
 };
 
-Array.prototype.every = function (arr, callbackFN) {
+Array.prototype.every = function (callbackFN) {
   // implementation
 
-  for (let i = 0; i < arr.length; i++) {
-    if (!callbackFN(arr[i], i, arr)) return false;
+  for (let i = 0; i < this.length; i++) {
+    if (!callbackFN(this[i], i, this)) return false;
   }
   return true;
 };
 
-Array.prototype.reduce = function (arr, callbackFN, initVal) {
+Array.prototype.reduce = function (callbackFN, initVal) {
   // implementation
 
   let acc = initVal;
   let startIndex = 0;
 
   if (initVal === undefined) {
-    acc = arr[0];
+    acc = this[0];
     startIndex = 1;
   }
 
-  for (let i = startIndex; i < arr.length; i++) {
-    acc = callbackFN(acc, arr[i], i, arr);
+  for (let i = startIndex; i < this.length; i++) {
+    acc = callbackFN(acc, this[i], i, this);
   }
 
   return acc;
 };
 
-Array.prototype.reduceRight = function (arr, callbackFN, initVal) {
+Array.prototype.reduceRight = function (callbackFN, initVal) {
   // implementation
 
   let acc = initVal;
-  let startIndex = arr.length - 1;
+  let startIndex = this.length - 1;
 
   if (!initVal) {
-    acc = arr[startIndex];
+    acc = this[startIndex];
     startIndex--;
   }
 
   for (startIndex; startIndex >= 0; startIndex--) {
-    acc = callbackFN(acc, arr[startIndex], startIndex, arr);
+    acc = callbackFN(acc, this[startIndex], startIndex, this);
   }
   return acc;
 };
 
-Array.prototype.join = function (arr, separator) {
+Array.prototype.join = function (separator) {
   // implementation
 
   let string = "";
   let sep = "";
 
-  for (let i = 0; i < arr.length; i++) {
-    string += sep + arr[i];
+  for (let i = 0; i < this.length; i++) {
+    string += sep + this[i];
     sep = separator;
   }
   return string;
 };
 
-Array.prototype.pop = function (arr) {
+Array.prototype.pop = function () {
   // implementation
 
-  const x = arr[arr.length - 1];
-  arr.length = arr.length - 1;
+  const x = this[this.length - 1];
+  this.length = this.length - 1;
   return x;
 };
 
-Array.prototype.unshift = function (arr, ...values) {
+Array.prototype.unshift = function (...values) {
   // implementation
 
-  const newArr = values.concat(...arr);
+  const newArr = values.concat(...this);
 
   for (let i = 0; i < newArr.length; i++) {
-    arr[i] = newArr[i];
+    this[i] = newArr[i];
   }
-  return arr.length;
+  return this.length;
 };

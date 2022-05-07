ae91d6b50989ebc9183ccbe4eafd46b9c88b8c3f
Mikayel <mikael.bejanyan@gmail.com>
2022-05-07 13:00:04 +0400

    Added ex2 partially completed

--- HW3/Ex2/script.js
+++ HW3/Ex2/script.js
@@ -8,7 +8,54 @@
 // the output should be
 // solution(chessPlayers, finishedMatches) = [[0, 3], [1, 3], [2, 3]]
 
+//// multiplyRange and combinChoose are for checking the size of the finishedMatches
+// helper for below function
+function multiplyRange(a, b) {
+  let product = a;
+  let i = a;
+
+  while (i++ < b) {
+    product *= i;
+  }
+  return product;
+}
+
+// n!/(r!*(n - r)!)
+function combinChoose(n, r) {
+  // base case for choose
+  if (n == r || r == 0) {
+    return 1;
+  } else {
+    if (r < n - r) {
+      r = n - r;
+    } else r = r;
+
+    return multiplyRange(r + 1, n) / multiplyRange(1, n - r);
+  }
+}
+// console.log(combinChoose(4, 2));
+// console.log(combinChoose(5, 2));
+// console.log(combinChoose(185, 33));
+
 const solution = (chessPlayers, finishedMatches) => {
-  // your code
+  if (finishedMatches.length === combinChoose(chessPlayers, 2)) {
+    return console.log("All matches are done");
+  }
+
+  // adding all the possible combination of games to the array below
+  let toBePlayed = [];
+  for (let i = 0; i < chessPlayers; i++) {
+    for (let j = i + 1; j < chessPlayers; j++) {
+      toBePlayed.push([i, j]);
+    }
+  }
+  console.log(toBePlayed);
 };
 
+const finishedMatches = [
+  [0, 1],
+  [1, 2],
+  [2, 0],
+];
+
+solution(4, finishedMatches);
